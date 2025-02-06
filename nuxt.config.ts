a87e98b15489c28568fae0242ad84da4493520d8
Lee Overy <me@leeovery.com>
2025-02-06 18:46:47 +0000

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,17 +5,6 @@ const { resolve } = createResolver(import.meta.url);
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-
-  components: [
-    {
-      path: resolve('./components'),
-      pathPrefix: false,
-    },
-  ],
-
-  // Commenting this out and changing the import inside the Button component to
-  // use a relative path over using the alias will make the error go away.
-  // So it seems using an alias is causing the issue.
   alias: {
     '@base': resolve('./'),
   },
