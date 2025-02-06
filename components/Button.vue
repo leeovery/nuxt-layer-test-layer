6792ec2fd302f7304d6f0e44e8c206c3589a23d6
Lee Overy <me@leeovery.com>
2025-02-06 18:53:30 +0000

    wip

--- components/Button.vue
+++ components/Button.vue
@@ -2,15 +2,19 @@
   import type { ButtonHTMLAttributes } from 'vue';
   // relative import works
   // import type { ThemeProps } from '../ui/createTheme';
+  // import type { ThemePropsEmpty } from '../ui/createTheme';
   // using alias causes issues with the type inference somehow.
-  import type { ThemeProps } from '@base/ui/createTheme';
+  // import type { ThemeProps } from '@base/ui/createTheme';
+  import type { ThemePropsEmpty } from '@base/ui/createTheme';
 
-  export interface ButtonProps extends ThemeProps<{ variants: string }> {
+  // export interface ButtonProps extends ThemeProps<{ variants: string }> {
+  //   type?: ButtonHTMLAttributes['type'];
+  //   disabled?: boolean;
+  // }
+  export interface ButtonProps extends ThemePropsEmpty {
     type?: ButtonHTMLAttributes['type'];
     disabled?: boolean;
   }
-
-  
 </script>
 
 <script lang="ts" setup>
--- ui/createTheme.ts
+++ ui/createTheme.ts
@@ -1,9 +1,13 @@
-type ThemeSlotKeys<T> = T extends { slots: infer S }
-                               ? { [K in keyof S]: string | undefined }
-                               : never;
+// type ThemeSlotKeys<T> = T extends { slots: infer S }
+//                                ? { [K in keyof S]: string | undefined }
+//                                : never;
 
-export interface ThemeProps<T> {
+// export interface ThemeProps<T> {
+//   theme?: string;
+//   themeClasses?: ThemeSlotKeys<T>;
+//   themeOverride?: T;
+// }
+
+export interface ThemePropsEmpty {
   theme?: string;
-  themeClasses?: ThemeSlotKeys<T>;
-  themeOverride?: T;
 }
